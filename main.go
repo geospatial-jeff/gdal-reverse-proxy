b74438be4d208013947cb05ce84e5c13472e5bfe
Jeff Albrecht <geospatialjeff@gmail.com>
2024-06-05 20:14:03 -0700

    expose proxy hostname through environment

--- main.go
+++ main.go
@@ -24,52 +24,6 @@ func copyHeader(dst, src http.Header) {
 	}
 }
 
-var group = groupcache.NewGroup("requests", 3<<20, groupcache.GetterFunc(
-	func(_ context.Context, key string, sink groupcache.Sink) error {
-		me, err := os.Hostname()
-		if err != nil {
-			panic("Get Hostname: " + err.Error())
-		}
-
-		log.Printf("Request handled by %s", me)
-
-		// Rebuild HTTP request from cache key
-		reader := bufio.NewReader(strings.NewReader(key))
-		originalRequest, err := http.ReadRequest(reader)
-		if err != nil {
-			panic(err)
-		}
-
-		// We can't have this set on client requests
-		originalRequest.RequestURI = ""
-
-		rawURL := "http://sentinel-cogs.s3.amazonaws.com"
-		if originalRequest.URL.Path != "" {
-			rawURL = rawURL + originalRequest.URL.Path
-		}
-		fullUrl, err := url.Parse(rawURL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		originalRequest.URL = fullUrl
-		originalRequest.Host = "sentinel-cogs.s3.amazonaws.com"
-
-		client := http.Client{}
-		res, err := client.Do(originalRequest)
-		if err != nil {
-			panic(err)
-		}
-
-		// Write HTTP response out to bytes, store in cache
-		var outBuf bytes.Buffer
-		if err := res.Write(&outBuf); err != nil {
-			log.Fatal(err)
-		}
-		sink.SetBytes(outBuf.Bytes(), time.Time{})
-		return nil
-	},
-))
-
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	// Serialize request to string (cache key)
 	var b = &bytes.Buffer{}
@@ -102,6 +56,56 @@ func newPool(peers []string) *groupcache.HTTPPool {
 	return pool
 }
 
+var group *groupcache.Group
+
+func newGroup(hostName string) {
+	group = groupcache.NewGroup("requests", 3<<20, groupcache.GetterFunc(
+		func(_ context.Context, key string, sink groupcache.Sink) error {
+			me, err := os.Hostname()
+			if err != nil {
+				panic("Get Hostname: " + err.Error())
+			}
+	
+			log.Printf("Request handled by %s", me)
+	
+			// Rebuild HTTP request from cache key
+			reader := bufio.NewReader(strings.NewReader(key))
+			originalRequest, err := http.ReadRequest(reader)
+			if err != nil {
+				panic(err)
+			}
+	
+			// We can't have this set on client requests
+			originalRequest.RequestURI = ""
+	
+			rawURL := "http://" + hostName
+			if originalRequest.URL.Path != "" {
+				rawURL = rawURL + originalRequest.URL.Path
+			}
+			fullUrl, err := url.Parse(rawURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			originalRequest.URL = fullUrl
+			originalRequest.Host = hostName
+	
+			client := http.Client{}
+			res, err := client.Do(originalRequest)
+			if err != nil {
+				panic(err)
+			}
+	
+			// Write HTTP response out to bytes, store in cache
+			var outBuf bytes.Buffer
+			if err := res.Write(&outBuf); err != nil {
+				log.Fatal(err)
+			}
+			sink.SetBytes(outBuf.Bytes(), time.Time{})
+			return nil
+		},
+	))	
+}
+
 func getPeers() []string {
 	me, err := os.Hostname()
 	if err != nil {
@@ -126,7 +130,8 @@ func getPeers() []string {
 }
 
 func main() {
-	// groupcache.SetLoggerFromLogger(newLogger())
+	proxyHostname := os.Getenv("PROXY_HOSTNAME")
+	newGroup(proxyHostname)
 
 	peers := getPeers()
 

e317e1c2dad712ca24e577c4e2ebca9aad51f6ff
Jeff Albrecht <geospatialjeff@gmail.com>
2024-06-05 20:04:45 -0700

    make it simpler, add nginx, use mailgun fork of groupcache

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module gdalproxy
 go 1.22.3
 
 require (
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/mailgun/groupcache/v2 v2.5.0 // indirect
+	github.com/segmentio/fasthash v1.0.3 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,30 +2,139 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"bytes"
+	"bufio"
 	"strings"
+	"time"
+	"io"
+	"github.com/mailgun/groupcache/v2"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-	lookup(ctx, w, r)
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+var group = groupcache.NewGroup("requests", 3<<20, groupcache.GetterFunc(
+	func(_ context.Context, key string, sink groupcache.Sink) error {
+		me, err := os.Hostname()
+		if err != nil {
+			panic("Get Hostname: " + err.Error())
+		}
+
+		log.Printf("Request handled by %s", me)
+
+		// Rebuild HTTP request from cache key
+		reader := bufio.NewReader(strings.NewReader(key))
+		originalRequest, err := http.ReadRequest(reader)
+		if err != nil {
+			panic(err)
+		}
+
+		// We can't have this set on client requests
+		originalRequest.RequestURI = ""
+
+		rawURL := "http://sentinel-cogs.s3.amazonaws.com"
+		if originalRequest.URL.Path != "" {
+			rawURL = rawURL + originalRequest.URL.Path
+		}
+		fullUrl, err := url.Parse(rawURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		originalRequest.URL = fullUrl
+		originalRequest.Host = "sentinel-cogs.s3.amazonaws.com"
+
+		client := http.Client{}
+		res, err := client.Do(originalRequest)
+		if err != nil {
+			panic(err)
+		}
+
+		// Write HTTP response out to bytes, store in cache
+		var outBuf bytes.Buffer
+		if err := res.Write(&outBuf); err != nil {
+			log.Fatal(err)
+		}
+		sink.SetBytes(outBuf.Bytes(), time.Time{})
+		return nil
+	},
+))
+
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	// Serialize request to string (cache key)
+	var b = &bytes.Buffer{}
+	if err := r.Write(b); err != nil {
+		log.Fatal(err)
+	}
+
+	// Hit the cache
+	var data []byte
+	if err := group.Get(r.Context(), b.String(), groupcache.AllocatingByteSliceSink(&data)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Serialize bytes into http response
+	buf := bytes.NewBuffer(data)
+	reader := bufio.NewReader(buf)
+	res, _ := http.ReadResponse(reader, r)
+
+	copyHeader(w.Header(), res.Header)
+	w.WriteHeader(res.StatusCode)
+	io.Copy(w, res.Body)
+	res.Body.Close()
+}	
+
+func newPool(peers []string) *groupcache.HTTPPool {
+	pool := groupcache.NewHTTPPoolOpts(peers[0], nil)
+	pool.Set(peers...)
+
+	return pool
+}
+
+func getPeers() []string {
+	me, err := os.Hostname()
+	if err != nil {
+		panic("Get Hostname: " + err.Error())
+	}
+
+	me = fmt.Sprintf("http://%s:8080", me)
+
+	peers := []string{
+		"http://app1:8080",
+		"http://app2:8080",
+		"http://app3:8080",
+	}
+
+	for i, v := range peers {
+		if v == me {
+			peers = append(peers[:i], peers[i+1:]...)
+		}
+	}
+
+	return append([]string{me}, peers...)
 }
 
 func main() {
-	// Setup groupcache
-	registerGroup("sentinel-cogs.s3.amazonaws.com")
-	peersConfig := os.Getenv("peers")
-	peers := strings.Split(peersConfig, ",")
-	pool := registerPeers(peers)
+	// groupcache.SetLoggerFromLogger(newLogger())
 
-	mux := http.NewServeMux()
-	mux.Handle("/", http.HandlerFunc(handler))
-	mux.Handle("/_groupcache/", pool)
-	log.Println(peers)
-	log.Println("starting server on :4000")
+	peers := getPeers()
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	log.Printf("listening on %v", peers[0])
+	log.Printf("peers: %v", peers)
+
+	http.HandleFunc("/", http.HandlerFunc(proxyHandler))
+	http.Handle("/_groupcache/", newPool(peers))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
